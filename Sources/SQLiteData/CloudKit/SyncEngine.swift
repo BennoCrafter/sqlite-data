872c1181ec38c0ffcc3e7c12d9b75fdf21a03d99
Benno <107504783+BennoCrafter@users.noreply.github.com>
2025-11-23 23:08:48 +0100

    Revert "Fix zone assignment for private and shareable tables"
    This reverts commit 062f47b424e8504db10c48c20540ff8294c7f60b.

--- Sources/SQLiteData/CloudKit/SyncEngine.swift
+++ Sources/SQLiteData/CloudKit/SyncEngine.swift
@@ -747,17 +747,7 @@
       var newChanges: [CKSyncEngine.PendingRecordZoneChange] = []
 
       let oldZoneID = CKRecordZone.ID(zoneName: oldZoneName, ownerName: oldOwnerName)
-      let zoneID: CKRecordZone.ID
-      if privateTables.contains(where: { $0.base.tableName == recordType }) {
-        // Force private tables to always use current user's zone
-        zoneID = CKRecordZone.ID(
-          zoneName: defaultZone.zoneName,
-          ownerName: CKCurrentUserDefaultName
-        )
-      } else {
-        // Use parent's zone for shareable tables
-        zoneID = parentZoneID ?? defaultZone.zoneID
-      }
+      let zoneID = CKRecordZone.ID(zoneName: zoneName, ownerName: ownerName)
 
       if oldZoneID != zoneID {
         oldChanges.append(.deleteRecord(CKRecord.ID(recordName: recordName, zoneID: oldZoneID)))
